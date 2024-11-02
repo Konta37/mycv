70670dee8f20ffeb0a63a1cfae796c713cbbf8b0
Duc Anh <137094647+Konta37@users.noreply.github.com>
2024-11-02 17:19:51 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const menu = document.getElementById("menu");
+const closeButton = document.getElementById("close-mobile");
+const nav = document.getElementById("nav-mobile");
+const navLink = document.querySelectorAll(".nav-link");
+
+menu.addEventListener("click", () => {
+  nav.classList.add("show");
+});
+
+closeButton.addEventListener("click", () => {
+  nav.classList.remove("show");
+});
+
+navLink.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("show");
+  });
+});
+
+function handleOpenCv() {
+  window.open(
+    "https://drive.google.com/file/d/1DE3T5kVlbR9OCv-ORqoDaqboyOIQKrC6/view?usp=drive_link",
+    "_blank"
+  );
+}
+function handleChangeToMyProject() {
+  window.location.href = "#project";
+}
